7a456db1c1af6ff858957fd41047958dbb606268
HyIAmHarshit <khandelwalharshit2024@gmail.com>
2026-04-22 22:22:17 +0530

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,48 @@
+public class Variables {
+    public static void main(String[] args) {
+        //create //declare
+        int age;
+        //System.out.println(age);
+        //assign value
+        age=10;
+        age=11;
+        System.out.println(age);
+
+        //define or intialize
+        int totalMarks =10;
+        System.out.println(totalMarks);
+
+        //case Sensitive
+        int weight =10;
+        int WEIGHT = 20;
+        System.out.println("weight: "+weight);
+        System.out.println("WEIGHT:"+WEIGHT);
+
+        //Starting Letter
+        int mark =10;
+        int MARKS=10;
+        int _mark=12;
+        int $mark=12;
+
+        //Subsequent Letter
+        int height5=25;
+        int height_love=12;
+        int height$12loves=12;
+        int height_=12;
+        int height$=22;
+
+        //No Reserved Kewords
+//        int class = 22;
+        //length
+        int hellojeekaisehosaremynameisharshit=101;
+        int a=1;
+        int c=3;
+        //camelCase
+        int totalMarkss=10;
+
+        //constants
+        int DAYS_IN_YEARS=365;
+
+
+    }
+}
